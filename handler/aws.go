9a71388d0a8a5037a89a3f830982db3c5b0b49e6
kagahehk01 <kagahe@hk01.com>
2019-02-23 21:01:17 +0800

    feat: add s3 regional endpoints to resove bucket name host

--- handler/aws.go
+++ handler/aws.go
@@ -27,6 +27,15 @@ func init() {
 		host := fmt.Sprintf("execute-api.%s.amazonaws.com", region)
 		services[host] = endpoints.ResolvedEndpoint{URL: fmt.Sprintf("https://%s", host), SigningMethod: "v4", SigningRegion: region, SigningName: "execute-api"}
 	}
+
+	// Add s3 endpoints
+	for region := range endpoints.AwsPartition().Regions() {
+		formats := []string{"s3-%s.amazonaws.com", "s3.%s.amazonaws.com"}
+		for _, format := range formats {
+			host := fmt.Sprintf(format, region)
+			services[host] = endpoints.ResolvedEndpoint{URL: fmt.Sprintf("https://%s", host), SigningMethod: "v4", SigningRegion: region, SigningName: "s3"}
+		}
+	}
 }
 
 func determineAWSServiceFromHost(host string) *endpoints.ResolvedEndpoint {
