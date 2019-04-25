ac2d08c110dfd88b6852b5bfbe2058f032ba3c07
brianbianco <brian.bianco@gmail.com>
2019-04-25 14:38:54 -0400

    Reworks how credentials are acquired so that it uses the aws sdk default chain
    The previous assembling of credentials meant that ECS task roles were not able to
    be used.  The new method just calls uses the aws sdk's default way of gathering
    credentials.  It now just passes the credentials out of that default config
    to the sigv4 sign method.  This should provide better flexibility.

--- main.go
+++ main.go
@@ -2,11 +2,6 @@ package main
 
 import (
 	"net/http"
-
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/credentials/ec2rolecreds"
-	"github.com/aws/aws-sdk-go/aws/ec2metadata"
-	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/aws/signer/v4"
 	"github.com/awslabs/aws-sigv4-proxy/handler"
   "github.com/aws/aws-sdk-go/aws/defaults"
@@ -19,32 +14,6 @@ var (
 	port  = kingpin.Flag("port", "port to serve http on").Default(":8080").String()
 )
 
-func getCredentials() (*credentials.Credentials, error) {
-	// Check env var, shared credentials, then finally ec2 instance role
-	providers := []credentials.Provider{
-		&credentials.EnvProvider{},
-		&credentials.SharedCredentialsProvider{},
-		&ec2rolecreds.EC2RoleProvider{
-			Client: ec2metadata.New(session.Must(session.NewSession())),
-		},
-	}
-
-  // If ECS task role endpoint is available add ECS task role to provider chain
-  if uri := os.Getenv("AWS_CONTAINER_CREDENTIALS_RELATIVE_URI"); len(uri) > 0 {
-    // Just create a default remote client for this?
-    providers = append(providers, defaults.RemoteCredProvider(&aws.Config{}, defaults.Handlers()))
-    log.Print("Remote cred provider added because ECS relative URI found")
-  }
-
-	creds := credentials.NewChainCredentials(providers)
-
-	if _, err := creds.Get(); err != nil {
-		return nil, err
-	}
-
-	return creds, nil
-}
-
 func main() {
 	kingpin.Parse()
 
@@ -53,10 +22,8 @@ func main() {
 		log.SetLevel(log.DebugLevel)
 	}
 
-	creds, err := getCredentials()
-	if err != nil {
-		log.WithError(err).Fatal("unable to get credentials")
-	}
+  cfg := defaults.Get().Config
+  creds := cfg.Credentials
 
 	signer := v4.NewSigner(creds)
 
