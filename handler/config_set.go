49331076f81685abedfdf3faeb637a071041cea8
k4r1 <16408267+k4r1@users.noreply.github.com>
2021-02-02 16:03:59 +0800

    Map yaml to struct explicitly with tags

--- handler/config_set.go
+++ handler/config_set.go
@@ -2,9 +2,9 @@ package handler
 
 // ConfigSet contains overrides for individual hosts
 type ConfigSet struct {
-	Name string
-	Region string
-	Host string
+	Name string    `yaml:"name"`
+	Region string  `yaml:"region"`
+	Host string    `yaml:"host"`
 	RoleArn string `yaml:"role-arn"`
 }
 
